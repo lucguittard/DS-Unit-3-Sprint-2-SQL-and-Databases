cf10d973f29a4edce6b2a2df2243d84988a94152
lucguittard <lucguittard@users.noreply.github.com>
2019-10-08 07:03:22 -0600

    sqlite commit

--- module1-introduction-to-sql/buddymove_holidayiq.py
+++ module1-introduction-to-sql/buddymove_holidayiq.py
@@ -1,11 +1,28 @@
+#DONE IN TERMINAL
 # import sqlite
 import sqlite3
 
+# calling the dataframe
+import pandas as pd
+dset = pd.read_csv('buddymove_holidayiq.csv')
+
+#Populate the database (sqlite)
+# make the engine
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://',echo=False)
+
 # establish a connection with the desired database
-conn = sqlite3.connect('buddymove_holidayiq.sqlite3')
+conn = sqlite3.connect('buddymove_holidayiq.sqlite3') 
+# connects the file to the database so queries can be run
 
+# make the sqlite file; note you need to specify the connection defined above
+dset.to_sql('buddymove_holidayiq', conn)
 
-# make queries
+# then cursor -> used for queries
+curs = conn.cursor()
+
+#DONE USING .py FILE
+# make queries (only part of this process that can be run from .py file)
 
 # query for number of rows
 query = 'SELECT COUNT(*) FROM buddymove_holiday;'
@@ -17,15 +34,12 @@ query2 = 'SELECT COUNT(DISTINCT(Id)) FROM buddymove_holiday WHERE Nature >= 100 
 
 # (Stretch): avg number of reviews per category
 
-# read in the appropiate file
-import pandas as pd
-dset = pd.read_csv('buddymove_holidayiq.csv')
-
-# make an engine
-from sqlalchemy import create_engine
-engine = create_engine('sqlite://',echo=False)
-
-# translate pandas dataframe to sql
-dset.to_sql('buddymove_holidayiq', con=engine)
+# translate pandas dataframe to sql; run in command line
 engine.execute(query).fetchall() 
 engine.execute(query2).fetchall()
+
+# close up each query
+curs.close
+
+# commit each query
+conn.commit()
